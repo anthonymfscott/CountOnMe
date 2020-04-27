5e11438369a23dc7f0f191c4d44f0987e33f1a67
anthonymfscott <anthonymfscott@gmail.com>
2020-04-27 19:11:12 +0200

    Update Expression.swift Add enum Sign Update result property: implement priority of operations

--- CountOnMe/Model/Expression.swift
+++ CountOnMe/Model/Expression.swift
@@ -8,9 +8,14 @@
 
 import Foundation
 
+// intérêt ?
+enum Sign: String {
+    case addition = "+", substraction = "-", multiplication = "x", division = "÷"
+}
+
 class Expression {
     var string = ""
-    
+
     var elements: [String] {
         return string.split(separator: " ").map { "\($0)" }
     }
@@ -34,29 +39,48 @@ class Expression {
         return string.firstIndex(of: "=") != nil
     }
     
+    // pas trop de logique pour une propriété calculée ?
     var result: String {
         var operationsToReduce = elements
+        
+        for sign in ["x", "÷"] {
+            while operationsToReduce.contains(sign) {
+                let signIndex = operationsToReduce.firstIndex(of: sign)!
+                
+                let left = Float(operationsToReduce[signIndex - 1])!
+                let sign = sign
+                let right = Float(operationsToReduce[signIndex + 1])!
+                
+                let result: Float
+                if sign == Sign.multiplication.rawValue {
+                    result = left * right
+                } else {
+                    guard right != 0 else { return "Erreur" }
+                    result = left / right
+                }
+                
+                for _ in 1...3 {
+                    operationsToReduce.remove(at: signIndex - 1)
+                }
+                
+                operationsToReduce.insert("\(result)", at: signIndex - 1)
+            }
+        }
         
         while operationsToReduce.count > 1 {
             let left = Float(operationsToReduce[0])!
             let sign = operationsToReduce[1]
             let right = Float(operationsToReduce[2])!
             
+            // utiliser même syntaxe que pour x et ÷ ci-dessus ? permettrait d'éviter le cas default
             let result: Float
             switch sign {
-                case "+":
-                    result = left + right
-                case "-":
-                    result = left - right
-                case "x":
-                    result = left * right
-                case "÷":
-                    guard right != 0 else {
-                        return "Erreur"
-                    }
-                    result = left / right
-                default:
-                    return "Opérateur inconnu !"
+            case "+":
+                result = left + right
+            case "-":
+                result = left - right
+            default:
+                return "Opérateur inconnu !"
             }
             
             operationsToReduce = Array(operationsToReduce.dropFirst(3))
@@ -64,7 +88,7 @@ class Expression {
         }
         
         let finalResult = simplify(operationsToReduce.first!)
-        
+        // directement appeler simplify dans return ?
         return finalResult
     }
     
