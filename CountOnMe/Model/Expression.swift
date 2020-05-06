623d9c1b99061a6ca27edef1794f6e831f8fe4b7
anthonymfscott <anthonymfscott@gmail.com>
2020-05-06 10:18:49 +0200

    Update Expression.swift Update Sign Change name of property string to input Add canAddPoint Update result Add comments

--- CountOnMe/Model/Expression.swift
+++ CountOnMe/Model/Expression.swift
@@ -1,88 +1,87 @@
 //
-//  SimpleCalc.swift
+//  Expression.swift
 //  CountOnMe
 //
 //  Created by anthonymfscott on 15/04/2020.
-//  Copyright © 2020 Vincent Saluzzo. All rights reserved.
+//  Copyright © 2020 Vincent Saluzzo - © 2020 anthonymfscott. All rights reserved.
 //
 
 import Foundation
 
-// intérêt ?
-enum Sign: String, CaseIterable {
-    case addition = "+", substraction = "-", multiplication = "x", division = "÷"
-    
+private enum Sign: String, CaseIterable {
+    case multiplication = "x", division = "÷", addition = "+", subtraction = "-"
+
     static var allCasesRawValues: [String] {
         return Sign.allCases.map { $0.rawValue }
     }
 }
 
 class Expression {
-    var string = ""
+    // MARK: Properties
+    var input = ""
 
     var elements: [String] {
-        return string.split(separator: " ").map { "\($0)" }
+        return input.split(separator: " ").map { "\($0)" }
     }
     
-    // Error check computed variables
     var isCoherent: Bool {
+        // if the expression finishes with a sign, return false; return true otherwise
         for sign in Sign.allCasesRawValues where elements.last == sign {
             return false
         }
         return true
     }
-    
+
     var hasEnoughElements: Bool {
         return elements.count >= 3
     }
-    
+
     var containsEqualsSign: Bool {
-        return string.firstIndex(of: "=") != nil
+        return input.contains("=")
     }
     
+    var canAddPoint: Bool {
+        return isCoherent && !elements.last!.contains(".")
+    }
+
     var result: String {
         var operationsToReduce = elements
-        
-        for signArray in ["x", "÷", "+", "-"] {
+
+        while operationsToReduce.count > 1 {
+            let signIndex = operationsToReduce.firstIndex(where: { $0 == "x" || $0 == "÷" }) ?? 1
             
-            while operationsToReduce.contains(signArray) {
-        
-                let signIndex = operationsToReduce.firstIndex(of: signArray)!
-                
-//                guard lets
-                let left = Float(operationsToReduce[signIndex - 1])!
-                let right = Float(operationsToReduce[signIndex + 1])!
-                let sign = Sign(rawValue: signArray)!
-                
-                let result: Float
-                switch sign {
-                case .addition:
-                    result = left + right
-                case .substraction:
-                    result = left - right
-                case .multiplication:
-                    result = left * right
-                case .division:
-                    guard right != 0 else { return "Erreur" }
-                    result = left / right
-//                default:
-//                    return "Opérateur inconnu !"
-                }
-                
-                for _ in 1...3 {
-                    operationsToReduce.remove(at: signIndex - 1)
-                }
-                
-                operationsToReduce.insert("\(result)", at: signIndex - 1)
-                
+            guard
+                let left = Float(operationsToReduce[signIndex - 1]),
+                let right = Float(operationsToReduce[signIndex + 1]),
+                let sign = Sign(rawValue: operationsToReduce[signIndex])
+                    else { return "Erreur d'opérateur" }
+
+            let result: Float
+            switch sign {
+            case .addition:
+                result = left + right
+            case .subtraction:
+                result = left - right
+            case .multiplication:
+                result = left * right
+            case .division:
+                guard right != 0 else { return "Erreur" }
+                result = left / right
             }
-    }
-        
+
+            for _ in 1...3 {
+                operationsToReduce.remove(at: signIndex - 1)
+            }
+
+            operationsToReduce.insert("\(result)", at: signIndex - 1)
+        }
+
         let finalResult = simplify(operationsToReduce.first!)
-        // directement appeler simplify dans return ?
+
         return finalResult
     }
-    
+
+    // MARK: Private methods
     private func simplify(_ floatNumber: String) -> String {
         let resultParts = floatNumber.split(separator: ".")
         if resultParts[1] != "0" {
